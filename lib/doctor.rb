7379ccd90c7c47b44f2b58b67b972ca3bfcc395e
Emilia ☆ <emilia.mena26@gmail.com>
2019-09-08 14:19:07 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -22,7 +22,7 @@ class Doctor
   end
  
   def patients 
-      Appointment.all.map {|a| a.patient}
+      Appointment.all.collect {|a| a.patient}
       # appointments.map do |appointment|
       # appointment.patients
     #end
